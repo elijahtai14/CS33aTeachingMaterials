087e056917a50ce8de1b10f6c4d54d12a2b92fbd
Elijah Tai <elijahtai@college.harvard.edu>
2020-02-12 05:08:00 -0500

    board

--- section2/sect2_board/tictactoe/views.py
+++ section2/sect2_board/tictactoe/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+board = [['_','X', 'O'], ['X', '_', 'O'], ['0', 'X', '_']]
 def index(request):
-	return render(request, "tictactoe/index.html")
+	return render(request, "tictactoe/index.html", {"board":board})
 	
